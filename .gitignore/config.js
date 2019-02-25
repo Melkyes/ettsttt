48546a554ed75800d6eb98daedc3237369a12e8c
Melkyes <47899743+Melkyes@users.noreply.github.com>
2019-02-25 19:38:58 +0100

    Update config.js

--- .gitignore/config.js
+++ .gitignore/config.js
@@ -1,5 +1,5 @@
-exports.D_TOKEN = 'NTQ5NjU4MDAxMzcyMzQ4NDI1.D1XEbg.jxdfnbNZqXyqJ4XrQ3GBzuEVk04';
+exports.TOKEN = token;
 
 exports.PREFIX = '*';
 
-exports.GOOGLE_API_KEY = 'AIzaSyBwnZsGdN8DJCtWzKTOpLiW6ilcbkscrE8';
+exports.GOOGLE_API_KEY = gak;
